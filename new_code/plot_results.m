7c78371a1ad65e53ae75424271ae2a44da85d314
Brian Hulette <hulettbh@vt.edu>
2013-11-14 22:34:52 -0500

    added simulate_and_record to run a simulation and record the results, and plot_results to display plots of those results

---
+++ new_code/plot_results.m
@@ -0,0 +1,48 @@
+function plot_results(glob)
+    close all;
+    
+    colors = ['b'; 'r'; 'g'];
+
+    results_dir = [pwd '/../results/'];
+    files = dir([results_dir glob '.mat']);
+    legend_data = cell(1,length(files));
+
+    for idx = 1:length(files)
+        filename = files(idx).name;
+        legend_data(idx) = {filename};
+        load([results_dir, filename])
+
+        figure(1);
+        if idx > 1
+            hold on;
+        end
+        semilogy(EBNO, ber, colors(mod(idx - 1, length(colors)) + 1));
+        if idx > 1
+            hold off;
+        end
+        
+        figure(2);
+        if idx > 1
+            hold on;
+        end
+        plot(EBNO, throughput, colors(mod(idx - 1, length(colors)) + 1));
+        if idx > 1
+            hold off;
+        end
+    end
+
+    figure(1);
+    hold on;
+    legend(legend_data);
+    title('BER')
+    xlabel('E_b/N_0 (dB)')
+    ylabel('BER')
+    hold off;
+
+    figure(2);
+    hold on;
+    legend(legend_data);
+    title('Throughput')
+    xlabel('E_b/N_0 (dB)')
+    ylabel('Throughput')
+    hold off;
