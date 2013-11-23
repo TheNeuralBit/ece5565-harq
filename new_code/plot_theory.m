fd46d9997f89f1c040fcda0307c72235677c71ad
Ruth Stoehr <ruth.stoehr@gmail.com>
2013-11-23 16:54:21 -0500

    Finished conv coding theoretical results for Type II.

--- new_code/plot_theory.m
+++ new_code/plot_theory.m
@@ -31,15 +31,37 @@ if cfg == 1 %With polys [15, 17]
     m = 4;
     alpha = [1 3 5 11 25 55 121 267 589 1299];
     dfree = 6;
+    
+    %For HARQII
+    dfree_3 = 10;
+    alpha_3 = [5 0 5 0 31 0 65 0 242 0 640 0 2058 0];
+    dfree_5 = 16;
+    alpha_5 = [1 3 1 0 3 2 1 9 14 6 11 29 22 25 76];
+    dfree_7 = 22;
+    alpha_7 = [1 0 3 0 1 0 3 0 3 0 9 0 14 0];
+    dfree_9 = 28;
+    alpha_9 = [1 0 0 3 0 0 1 0 3 0 1 2 0 9 0];
+    
 elseif cfg == 2 %With polys [133, 171]
     CODE_RATE = 1/2; 
     GENERATING_POLYS = [133 171]; %Rate 1/2
     CONSTRAINT_LENGTH = 6; 
-    k = 960;
-    n_p = 14;
+    k = 2048;%960;
+    n_p = 32;%14;
     m = 6;
     alpha = [11 0 38 0 193 0 1331 0 7275 0];
     dfree = 10;
+    
+    %For HARQII
+    dfree_3 = 13;
+    alpha_3 = [1 1 5 6 9 21 35 50 88 203 339 691];
+    dfree_5 = 23;
+    alpha_5 = [1 1 4 3 2 4 8 15 10 6 24 41 45 54];
+    dfree_7 = 33;
+    alpha_7 = [1 1 4 3 1 1 1 3 8 15 10 1 0 5 24];
+    dfree_9 = 43;
+    alpha_9 = [1 1 4 3 1 1 0 0 1 3 8 15 10 1 0];
+    
 else
     error('Invalid input to function.')
 end
@@ -75,22 +97,58 @@ Thr_I = 0.5./Tr_I .* (k./(k+n_p+m)); %Assumes l = 1
 %% HARQ Type II Throughput
 R = 1 - (1 - Q(ebno,2)).^(k+n_p + m); %Assumes l = 1
 
-%NEED TO FINISH THIS!!!
+P_E_2 = 0;
+for idx=1:length(alpha)
+    P_E_2 = P_E_2 + alpha(idx).*Q(ebno,2*(dfree+idx-1));
+end 
+P_D_2 = 1 - (1-P_E_2).^(k+n_p); %Assumes l = 1
 
-%P_E = 0;
-%for idx=1:length(alpha)
-%    P_E = P_E + alpha(idx).*Q(ebno,2*(dfree+idx-1));
-%end 
+P_E_3 = 0;
+for idx=1:length(alpha_3)
+    P_E_3 = P_E_3 + alpha_3(idx).*Q(ebno,2*(dfree_3+idx-1));
+end 
+P_D_3 = 1 - (1-P_E_3).^(k+n_p); %Assumes l = 1
+
+P_E_5 = 0;
+for idx=1:length(alpha_5)
+    P_E_5 = P_E_5 + alpha_5(idx).*Q(ebno,2*(dfree_5+idx-1));
+end 
+P_D_5 = 1 - (1-P_E_5).^(k+n_p); %Assumes l = 1
+
+P_E_7 = 0;
+for idx=1:length(alpha_7)
+    P_E_7 = P_E_7 + alpha_7(idx).*Q(ebno,2*(dfree_7+idx-1));
+end 
+P_D_7 = 1 - (1-P_E_7).^(k+n_p); %Assumes l = 1
+
+P_E_9 = 0;
+for idx=1:length(alpha_9)
+    P_E_9 = P_E_9 + alpha_9(idx).*Q(ebno,2*(dfree_9+idx-1));
+end 
+P_D_9 = 1 - (1-P_E_9).^(k+n_p); %Assumes l = 1
+
+
+P_D = P_D_2 + P_D_3 + P_D_5 + P_D_7 + P_D_9;
+Tr_II = 1 + R + P_D;
+Thr_II = 1./Tr_II .* (k./(k+n_p+m)); %Assumes l = 1
+
 
 
 
 %% Plot
 
+%Fix approximations of HARQII to match ARQ if ARQ has better performance
+for idx=1:length(Thr_II)
+    if Thr_ARQ(idx) > Thr_II(idx)
+        Thr_II(idx) = Thr_ARQ(idx);
+    end
+end
+
 %Plot on log scale
 figure;
-plot(EBNO,Thr_ARQ,'-ks',EBNO,Thr_I,'-m*')
+plot(EBNO,Thr_ARQ,'-ks',EBNO,Thr_I,'-r*',EBNO,Thr_II, '-bo')
 axis([min(EBNO) max(EBNO) 0 1])
-legend('ARQ','Type I HARQ upper bound')
+legend('ARQ','Type I HARQ upper bound','Type II HARQ upper bound','Location','NorthWest')
 xlabel('Eb/No (dB)')
 ylabel('Throughput')
 title('Throughput for ARQ, Type I HARQ, and Type II HARQ with code combining') %assumes convolutional coding with soft decisions
